e07b2024d49fc52660496b0ed02c849a102b0ab3
Lambert <lambertgithub@gmail.com>
2022-03-08 14:20:18 +0200

    Installing axios

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios'
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [users , setUsers] = useState([])
+  const [isLoading , setIsLoading] = useState(false)
+
+  useEffect(()=>{
+    const fetchUsers = async() =>{
+      setIsLoading(true)
+      const { data:users } = await axios.get('https://jsonplaceholder.typicode.com/users')
+      setUsers(users)
+      setIsLoading(false)
+     }
+     fetchUsers()
+  },[])
+  return (
+    <div className="App">
+       <h2>Users List</h2>
+       {
+         isLoading && <h2>Please wait ....</h2>
+       }
+       {
+         users.map(user=>{
+           return <h2>{user.name}</h2>
+         })
+       }
+    </div>
+  );
+}
+
+export default App;
